f2695d67872141bee0e589295d4dee93c18f7075
ernane <ernanecarvalho89@gmai.com>
2022-11-15 11:54:09 -0300

    getProducts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,7 +4,7 @@ import * as apiController from '../controllers/apiController';
 const router = Router();
 
 router.get('/ping', apiController.ping);
-
 router.post('/product', apiController.createProduct);
+router.get('/products', apiController.getProducts);
 
 export default router;
